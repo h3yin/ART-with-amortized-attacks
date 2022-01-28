11c541e49ddc7e3c6144acb6219652e5fd3b165d
Haikuo Yin <kevrev0@gmail.com>
2022-01-28 01:25:04 -0800

    modified run_thresh.sh

--- run_thresh.sh
+++ run_thresh.sh
@@ -1,4 +1,4 @@
-python tests/amortized/amortized_attack.py 10000 20 10000 32 10000 0 0 amortized FGSM 1
-python tests/amortized/amortized_attack.py 10000 20 10000 32 5000 0 0 amortized FGSM 1
-python tests/amortized/amortized_attack.py 10000 20 10000 32 1000 0 0 amortized FGSM 1
-python tests/amortized/amortized_attack.py 1000 20 1000 32 1000 0 0 amortized FGSM 1
+python tests/amortized/amortized_attack.py 10000 20 10000 16 10000 0 0 amortized FGSM 1
+python tests/amortized/amortized_attack.py 10000 20 10000 16 5000 0 0 amortized FGSM 1
+python tests/amortized/amortized_attack.py 10000 20 10000 16 1000 0 0 amortized FGSM 1
+python tests/amortized/amortized_attack.py 1000 20 1000 16 1000 0 0 amortized FGSM 1
